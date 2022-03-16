1e03ab45284499c7a87a0cb8f397ae5b3196fb58
Narcisah <91091964+mel-ificent@users.noreply.github.com>
2022-03-16 17:17:38 -0400

    updated user controller

---
+++ controllers/userController.js
@@ -0,0 +1,95 @@
+const { User, Thought } = require('../models');
+
+const reaction = async (username) =>
+  User.aggregate([
+    {
+      $unwind: '$reactions',
+    },
+    {
+      $group: { _id: username, reaction: reactionBody },
+    },
+  ]);
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObj = {
+          users,
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.username })
+      .select('-__v')
+      .then(async (user) =>
+        !student
+          ? res.status(404).json({ message: 'No user with that username' })
+          : res.json({
+              user,
+              reaction: await reaction(req.params.username),
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Delete a user 
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.username })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No such user exists' })
+          : Thought.findOneAndUpdate(
+              { users: req.params.username },
+              { $pull: { users: req.params.username } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: 'User deleted, but no thoughts found',
+            })
+          : res.json({ message: 'User successfully deleted' })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Add an assignment to a student
+  addThought(req, res) {
+    console.log('You are adding a thought');
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.username },
+      { $addToSet: { thoughts: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+};
